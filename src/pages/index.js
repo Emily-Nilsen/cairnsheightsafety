8d89ecccb560603a7645111d1ce7f3b4e1b25ea5
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-05-21 16:07:00 +0200

    edit meta data description

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,14 +20,14 @@ export default function Home() {
     <>
       <Head>
         <title>
-          Cairns Height Safety - We provide nationally accredited Height Safety
-          and Rescue training and Rope Access services
+          Cairns Height Safety - Expert Rope Access Services and IMMOOS Cable
+          System Solutions
         </title>
         <meta
           name="description"
-          content="Cairns Height Safety provide nationally accredited Height Safety
-          and Rescue training and Rope Access services. "
+          content="Cairns Height Safety specializes in expert Rope Access Services and innovative IMMOOS cable system solutions to meet all your height safety needs."
         />
+
         <meta
           name="keywords"
           content="Cairns, FNQ, Queensland, Far North Queensland, Tablelands, Industrial Rope Access services, Industrial Rope Access, Rope Access services, rope services, Wind turbine maintenance, Mould remediation, High-pressure water blasting, Window cleaning, Exterior building maintenance, Installation of Height Safety Systems, Minor roof and gutter repairs, Qualified riggers, training centre, training courses, Gordonvale, Edmonton, Northern Beaches, Atherton, Ravenshoe, Kuranda, Smithfield, Palm Cove, Mossman, Cape Tribulation, Northern Cairns, Cairns City, Esplanade"
